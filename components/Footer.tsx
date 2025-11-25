d492c914afa55ca740b7d2e26a8678563106bb39
James <62422157+caetismus@users.noreply.github.com>
2025-11-25 13:48:20 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,7 +7,6 @@ const Footer: React.FC = () => {
     <footer className="bg-slate-950 text-slate-400 py-12 border-t border-slate-900">
       <div className="container mx-auto px-6">
         <div className="flex flex-col md:flex-row justify-between items-center">
-          
           <div className="flex space-x-6">
             <a 
               href="mailto:jamescubito@gmail.com" 
